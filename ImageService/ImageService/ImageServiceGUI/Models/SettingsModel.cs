71568e0cad28d0d701de0903c717dad06bb3fccb
LeeAlima <leealima28@gmail.com>
2018-05-06 23:48:38 +0300

    Working on the MVVM, VMSettings is done! :) left VMLOG and the models!

---
+++ ImageService/ImageService/ImageServiceGUI/Models/SettingsModel.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace ImageServiceGUI.Models
+{
+    class SettingsModel : ISettingsModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        //public ObservableCollection<string> Handlers { get; set; }
+
+        public SettingsModel()
+        {
+            Handlers = new ObservableCollection<string>();
+            Handlers.Add("Lee");
+            Handlers.Add("Omi");
+
+        }
+
+        // ADD ALL THE COMMUNICATION WITH THE SERVER!!!!!!!!!!!!
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        // ADD C'TOR THAT COMMUNICATE WITH THE SERVER!!!!!!
+
+        private string m_OutputDirectory = "aaa";
+        public string OutputDirectory
+        {
+            get { return this.m_OutputDirectory; }
+            set
+            {
+                m_OutputDirectory = value;
+                NotifyPropertyChanged("OutputDirectory");
+            }
+        }
+
+        private string m_SourceName = "bbb";
+        public string SourceName
+        {
+            get { return this.m_SourceName; }
+            set
+            {
+                m_SourceName = value;
+                NotifyPropertyChanged("SourceName");
+            }
+        }
+
+        private string m_LogName = "ccc";
+        public string LogName
+        {
+            get { return this.m_LogName; }
+            set
+            {
+                m_LogName = value;
+                NotifyPropertyChanged("LogName");
+            }
+        }
+
+        private int m_ThumbnailSize = 120;
+        public int ThumbnailSize
+        {
+            get { return this.m_ThumbnailSize; }
+            set
+            {
+                m_ThumbnailSize = value;
+                NotifyPropertyChanged("ThumbnailSize");
+            }
+        }
+        public ObservableCollection<string> Handlers { get; set; }
+
+    }
+}
---
+++ ImageService/ImageService/ImageServiceLogging/LogEntry.cs
@@ -0,0 +1,21 @@
+﻿using ImageServiceInfrastructure.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageServiceLogging
+{
+    public class LogEntry
+    {
+        public string msg { get; set; }
+        public MessageTypeEnum type { get; set; }
+
+        public LogEntry(MessageTypeEnum type, string message)
+        {
+            this.type = type;
+            this.msg = message;
+        }
+    }
+}
