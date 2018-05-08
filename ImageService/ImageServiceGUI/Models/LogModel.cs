2dece8736caa8cea7d22ced1dc82f828c98807a1
LeeAlima <leealima28@gmail.com>
2018-05-08 16:48:55 +0300

    New version

---
+++ ImageService/ImageServiceGUI/Models/LogModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using ImageServiceLogging;
+
+namespace ImageServiceGUI.Models
+{
+    class LogModel : ILogModel
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<LogEntry> m_Messages;
+        public ObservableCollection<LogEntry> Messages
+        {
+            get { return this.m_Messages; }
+            set
+            {
+                this.m_Messages = value;
+                this.NotifyPropertyChanged("Messages");
+            }
+        }
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
